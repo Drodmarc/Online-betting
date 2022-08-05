394947b8d363c26d76b164512f62664cff0afc10
Drodmarc <rodmarc1207@gmail.com>
2022-08-05 11:26:13 +0800

    [Feat #3] Create template for users view

---
+++ app/controllers/users/profiles_controller.rb
@@ -0,0 +1,3 @@
+class Users::ProfilesController < ApplicationController
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-
-  root to: 'home#index'
   constraints(ClientDomainConstraint.new) do
+    devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+    root to: 'home#index'
+
+    namespace :users do
+      resource :profile
+    end
   end
 
   constraints(AdminDomainConstraint.new) do
