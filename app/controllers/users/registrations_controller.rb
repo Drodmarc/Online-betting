80560b95a4e23690e9fe57da5ff879f9f63e08fe
Drodmarc <rodmarc1207@gmail.com>
2022-08-08 14:33:56 +0800

    [Feat #4] User profile edit page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,40 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
-  def new
-    super
+  def update
+    user=params[:user]
+    if user[:current_password].blank? && user[:password_confirmation].blank? && user[:password].blank?
+      update_user
+    else
+      update_password
+    end
   end
 
-  def create
-    super
+  private
+
+  def update_password
+    if @user.update_with_password(user_password_params)
+      flash[:notice] = "Successfully Updated"
+      sign_in @user, :bypass => true
+      redirect_to users_profile_path
+    else
+      render :edit
+    end
+  end
+
+  def update_user
+    if @user.update(user_details_params)
+      flash[:notice] = "Successfully Updated"
+      sign_in @user, :bypass => true
+      redirect_to users_profile_path
+    else
+      render :edit
+    end
+  end
+
+  def user_details_params
+    params.require(:user).permit(:phone, :image, :username)
+  end
+
+  def user_password_params
+    params.require(:user).permit(:phone, :image, :username, :password, :password_confirmation, :current_password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :phone, phone: {allow_blank: true}
+  mount_uploader :image, ImageUploader
 end
