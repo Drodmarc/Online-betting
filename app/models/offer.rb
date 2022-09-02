cd0187da8c82974ffa8d23ac1a6eb5034db9cbd6
Drodmarc <rodmarc1207@gmail.com>
2022-09-02 10:34:25 +0800

    [Feat 20] Order Model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
   validates :image, :name, :genre, :status,  presence: true
   validates :amount, :coin, numericality: { greater_than: 0 }
-
+  has_many :orders
   mount_uploader :image, ImageUploader
 
   enum status: [:active, :inactive]
