9ee22267a1d8c51c3c1cecea1b7634f60f032233
Drodmarc <rodmarc1207@gmail.com>
2022-09-14 13:12:43 +0800

    [Feat 34] Display banner and news_ticker

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     return if address.nil?
     "#{address.street_address}, #{address.barangay.name}, #{address.city.name}, #{address.province.name}, #{address.region.name}"
   end
+
+  def banners
+    Banner.where('online_at <= ? AND offline_at > ?', Time.now, Time.now).active
+  end
+
+  def news_tickers
+    NewsTicker.active.limit(5)
+  end
 end
